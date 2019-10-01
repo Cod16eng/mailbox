45ec2140a3b187b85c60008e3377eda20fc6aff1
Codrin Popescu <codrin@ascheri.co.uk>
2019-10-01 16:28:55 +0100

    added advanced search model

--- app/models/search.rb
+++ app/models/search.rb
@@ -6,7 +6,7 @@ class Search < ApplicationRecord
 	private
 
 	def find_posts
-	  posts = Post.joins(:company).order(:name)
+	  posts = Post.includes(:company).order('companies.name')
 	  posts = posts.where("name like ?", "%#{name}%") if name.present?
 	  posts = posts.where(company_id: company_id) if company_id.present?
 	  posts = posts.where("sender like ?", "%#{sender}%") if sender.present?
